c225cf2fd0f7aba04abf9e314d32773066400fe9
Haywhice <lalaayomideisaac@gmail.com>
2023-01-08 12:12:23 -0800

    update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,10 +12,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		*s(i + 1) = b;
-		i += 1;
+		s[i] = b;
 	}
 	return (s);
 }
+
