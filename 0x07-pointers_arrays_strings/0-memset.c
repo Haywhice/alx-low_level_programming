4fb8999109bb57cb80cea3e362efc20f34683dbc
Haywhice <lalaayomideisaac@gmail.com>
2023-01-08 14:20:04 -0800

    Update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  * _memset - ironnou
- * @s: pointer to char params
+ *@s: pointer to char params
  * @b: data to change
- * @n: index
- * Return: *s
+ *@n: index
+ *Return: *s
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -18,4 +17,3 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
-
