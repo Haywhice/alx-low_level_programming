09a6e0c625174603aa399d37b4035b63db2c753d
Haywhice <Lalaayomideisaac@gmail.com>
2023-06-20 16:34:55 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - structure dog
+ * @name: first member
+ * @age: Second member
+ * @owner: third member
+ *
+ * Description: longer description
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
