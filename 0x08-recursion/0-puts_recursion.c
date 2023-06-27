9555039cd2bf7352ec58d086415941a4e5892bf0
“Haywhice” <Lalaayomideisaac@gmail.com>
2023-06-27 08:44:30 -0700

    Task_0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,22 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: pointer to the string
- * Return: void
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
-
-
 void _puts_recursion(char *s)
 {
-
-        if (*s == '\0')
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                _putchar(*s);
-                _puts_recursion(s + 1);
-        }
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
